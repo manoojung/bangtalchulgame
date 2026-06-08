1ab505188024a2a87fa9bdd9496231622eed7268
manoojung <cheonujeong5@gmail.com>
2026-06-08 23:05:39 +0900

    Add files via upload

---
+++ assets.js
@@ -0,0 +1,22 @@
+// 이미지, 사운드 변수
+let bgImg, menuBtnImg1, menuBtnImg2;
+let introImgs = [];
+const totalIntroImgs = 6;
+
+let dia;
+let bg;
+
+let char1, char2, char3, char4, char5, char6, char7, char8;
+let char9, char10, char11, char12, char13, char14;
+
+let imgWardrobe, imgSkirt, imgHairpin, imgDrawer, imgStatue;
+let imgBlanket, imgAxe, imgUrinal, imgOrchid, imgNovel;
+let imgLockDoor;
+
+let breakSound, mouse, manSound, paperSound, doorSound, findSound;
+let metalClang, drawerOpen, unlockSound, dialSound;
+let crowdSound, paperFallBig, paperFallSmall;
+
+let end3, end3_1, happyEndImg, stampImg, happyStampImg;
+
+let crySound, stampSound, hitSound;
